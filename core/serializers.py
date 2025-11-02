ad9b682f1d2276aafd652a7ef99c8555d70ded62
Genius-Excel <geniusexcel9@gmail.com>
2025-11-03 00:42:10 +0100

    Refactor UserRegistrationSerializer to ensure email field is required

--- core/serializers.py
+++ core/serializers.py
@@ -3,4 +3,6 @@ from rest_framework import serializers
 
 class UserRegistrationSerializer(serializers.Serializer):
     nin = serializers.CharField(required=False)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField()
+    phone_number = serializers.CharField(max_length=100)
+    password = serializers.CharField(max_length=100)
