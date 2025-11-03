ecbe1183f0115f4764add52534ad54eae10623aa
Genius-Excel <geniusexcel9@gmail.com>
2025-11-03 01:21:37 +0100

    Add login and logout paths to URL configuration

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('register/<str:user_type>', views.register_user, name='register-user'),
+    path('auth/login', views.login_user, name='login-user'),
+    path('auth/logout', views.logout_user, name='logout-user'),
 ]
