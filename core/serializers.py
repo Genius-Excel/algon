b61bcb99c32467e05d0ca7af29c0c9ac719de0c4
Genius-Excel <geniusexcel9@gmail.com>
2025-11-03 01:21:30 +0100

    Refactor serializers to maintain consistent formatting and add new line at the end of the file

--- core/serializers.py
+++ core/serializers.py
@@ -6,3 +6,15 @@ class UserRegistrationSerializer(serializers.Serializer):
     email = serializers.EmailField()
     phone_number = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=100)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    nin = serializers.CharField(required=False)
+    password = serializers.CharField(max_length=100)
+
+
+class UserLogoutSerializer(serializers.Serializer):
+    access_token = serializers.CharField(required=True)
+    refresh_token = serializers.CharField(required=True)
+
